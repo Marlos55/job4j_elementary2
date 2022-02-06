0bcd6c05a6a1d04c065b3ac0d5bdbf5b87f63cf9
Aleksandr <losas20@yandex.ru>
2022-02-06 16:42:25 +0600

    4.1.2. Глупый бот.Тесты.

---
+++ src/test/java/ru/job4j/DummyBotTest.java
@@ -0,0 +1,33 @@
+package ru.job4j;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DummyBotTest {
+
+    @Test
+    public void whenGreetBot() {
+        String in = "Привет, Бот.";
+        String res = DummyBot.answer(in);
+        String exp = "Привет, умник.";
+        Assert.assertEquals(exp, res);
+    }
+
+    @Test
+    public void whenByeBot() {
+        String in = "Пока.";
+        String res = DummyBot.answer(in);
+        String exp = "До скорой встречи.";
+        Assert.assertEquals(exp, res);
+    }
+
+    @Test
+    public void whenUnknownBot() {
+        String in = "Сколько будет 2 + 2?";
+        String res = DummyBot.answer(in);
+        String exp = "Это ставит меня в тупик. Задайте другой вопрос";
+        Assert.assertEquals(exp, res);
+    }
+}
